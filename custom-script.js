15827627106cadb10d98eb38a18ed7b796d2fbc7
yerinHan <yerinhan2004@gmail.com>
2025-11-27 17:48:19 +0900

    create a new project

---
+++ custom-script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // 버튼 클릭 시 알림 메시지 표시
+    const alertButton = document.getElementById("alert-button");
+      alertButton.addEventListener("click", function() {
+         alert("Button clicked!");
+     });
+
+    // "Learn more" 버튼 클릭 시 learn.html로 이동
+    // alertButton.addEventListener("click", function () {
+    //     window.location.href = "learn-more.html";
+    // });
+
+    
+   
+    // 이미지 변경
+    const imageElement = document.getElementById("change-image");
+    const originalImageSrc = imageElement.src;
+    const newImageSrc = "./images/mountain.jpg";
+
+    imageElement.addEventListener("mouseover", function() {
+        imageElement.src = newImageSrc;
+    });
+
+    imageElement.addEventListener("mouseout", function() {
+        imageElement.src = originalImageSrc;
+    });
+
+    
+});
